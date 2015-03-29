1ef510499dc04aaa725675b3bad585bec684f230
Helen See <helen.see@rutgers.edu>
2015-03-29 02:03:56 -0400

    added score
    scores visible ish

---
+++ toggle/Assets/levelOne/Scores.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using llamaStatus;
+
+public class Scores : MonoBehaviour {
+	
+	public int currentWater;
+	Text waterDisplay;
+	// Use this for initialization
+	void Awake(){
+		waterDisplay = GetComponent<Text> ();
+		currentWater = 0;
+	}
+	// Update is called once per frame
+	void Update () {
+		waterDisplay.text = "Score: " + currentWater;
+	}
+}
---
+++ toggle/Assets/levelOne/llamaStatus.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+namespace llamaStatus{
+	public class llamaStatus : MonoBehaviour {
+
+		public int totalWater { get; set; }
+		public int totalDirt {get; set;}
+	}
+}
