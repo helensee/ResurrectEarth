c2dbb97f159fd181b9f9bd0c60c608c339b05fb9
Helen See <helen.see@rutgers.edu>
2015-03-29 12:43:21 -0400

    changes done
    edited scores, fixed llama, mouse si

--- toggle/Assets/levelOne/Scores.cs
+++ toggle/Assets/levelOne/Scores.cs
@@ -1,12 +1,22 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
 using System.Collections;
+using UnitySampleAssets.CrossPlatformInput;
 
 public class Scores : MonoBehaviour {
 
 	public static int totalWater { get; set; }
 	public static int totalDirt { get; set; }
 
+	private Vector2 scale = new Vector2(1, 1);
+	private Vector2 pivotPoint;
+	void OnGUI() {
+		pivotPoint = new Vector2(Screen.width - 50, 1);
+		GUIUtility.ScaleAroundPivot(scale, pivotPoint);
+		if (GUI.Button (new Rect (Screen.width / 2 - 310, Screen.height / 2 - 120, 100, 30), "Score: " + totalWater)) {
+		}
+		
+	}
 	Text waterDisplay;
 
 	// Use this for initialization
@@ -15,6 +25,6 @@ public class Scores : MonoBehaviour {
 	}
 	// Update is called once per frame
 	void Update () {
-		waterDisplay.text = "Score: " + totalWater;
+
 	}
 }
