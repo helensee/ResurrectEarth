a1afa3f085a14bcd65bf740fee1d65477aba50e1
Helen See <helen.see@rutgers.edu>
2015-03-28 22:24:16 -0400

    current project
    llama moves/jumps, platform, background

---
+++ toggle/Assets/SampleAssets/2D/Scripts/llamaBlue.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnitySampleAssets.CrossPlatformInput;
+
+public class llamaBlue : MonoBehaviour {
+	public Animator blueAnim;
+
+	// Use this for initialization
+	void Start () {
+		blueAnim = GetComponent<Animator> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float move = CrossPlatformInputManager.GetAxis("Horizontal");
+		if (move == 0) {
+			blueAnim.enabled = false;
+		}
+		blueAnim.enabled = true;
+	}
+}
