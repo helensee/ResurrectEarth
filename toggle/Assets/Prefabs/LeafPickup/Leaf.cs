1e6a986173cf15baec5482795310b12feb5f301c
Rosheen <rosheenmail@gmail.com>
2015-03-29 01:58:36 -0400

    added water

---
+++ toggle/Assets/Prefabs/LeafPickup/Leaf.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Leaf : MonoBehaviour 
+{
+    //Leaf will appear once this timer reaches 0
+    private float displayTimer = 1.0f;
+
+    void FixedUpdate()
+    {
+        print(displayTimer); //print timer to console
+        if (displayTimer > 0)
+        {
+            displayTimer -= Time.deltaTime;
+        }
+        else
+        {
+            //Timer reached 0, so display object
+            this.gameObject.GetComponent<SpriteRenderer>().enabled = true;
+            this.gameObject.GetComponent<BoxCollider2D>().enabled = true;
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            //Call addPoints method in score script and destroy this object
+
+            Destroy(this.gameObject);
+        }
+    }
+}
